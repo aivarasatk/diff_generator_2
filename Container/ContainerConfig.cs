c6a26f5c89feb1a3ba5d053d8a6f09bac2076a52
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-03-24 14:59:50 +0200

    initial project files

---
+++ Container/ContainerConfig.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffGenerator2.Container
+{
+    public class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            //register types
+
+            return builder.Build();
+        }
+    }
+}
