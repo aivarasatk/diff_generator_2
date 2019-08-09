0df9d61395b02b2becb48c90e4baa913ddd3ad89
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-08-09 14:51:54 +0300

    UI refactor and improvements

---
+++ Constants/DialogIcon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffGenerator2.Constants
+{
+    public class DialogIcon
+    {
+        public const string FinishedFilled = "CheckboxMarkedCircle";
+        public const string InformationFilled = "About";
+        public const string WarningFilled = "WarningOctagon";
+        public const string ErrorFilled = "Error";
+    }
+}
---
+++ Model/InformationDialogModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DiffGenerator2.Model
+{
+    public class InformationDialogModel : INotifyPropertyChanged
+    {
+        private string _icon;
+        private string _message;
+
+        public string Icon
+        {
+            get { return _icon; }
+            set
+            {
+                this.MutateVerbose(ref _icon, value, RaisePropertyChanged());
+            }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                this.MutateVerbose(ref _message, value, RaisePropertyChanged());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged()
+        {
+            return args => PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
