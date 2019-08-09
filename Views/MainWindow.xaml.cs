dd25e8a9346ef7f8aeeaa898dd7dee5c36391388
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-08-09 15:12:24 +0300

    Added global exception handler

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using DiffGenerator2.Common;
 using DiffGenerator2.Interfaces;
 using DiffGenerator2.ViewModel;
+using System;
 using System.Windows;
 
 namespace DiffGenerator2.Views
@@ -14,9 +15,16 @@ namespace DiffGenerator2.Views
         public MainWindow()
         {
             InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalExceptionHandler);
             var configuration = ContainerConfig.Configure();
             var mainViewModel = configuration.Resolve<MainViewModel>();
             DataContext = mainViewModel;
+        }
+
+        private static void GlobalExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception ex = (Exception)args.ExceptionObject;
+            MessageBox.Show($"Įvyko nenumatyta klaida, programa bus išjungta.{Environment.NewLine}{ex}", "Globali klaida", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
